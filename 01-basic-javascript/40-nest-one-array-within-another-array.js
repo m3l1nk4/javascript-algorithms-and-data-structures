ab869b5bfe5c5a99f1c40f3f353e4ab32c18bb4c
メ <111341694+m3l1nk4@users.noreply.github.com>
2023-05-08 13:44:48 +0000

    Add files

---
+++ 01-basic-javascript/40-nest-one-array-within-another-array.js
@@ -0,0 +1,2 @@
+// Only change code below this line
+const myArray = [["rainbow", 7], ["cloud", 9]];
---
+++ 01-basic-javascript/48-shopping-list.js
@@ -0,0 +1,7 @@
+const myList = [
+  ["Smoked Salmon", 1],
+  ["Soy Milk", 2],
+  ["Mixed Cheese", 1],
+  ["Matcha Ice Cream", 5],
+  ["English Breakfast Tea", 1],
+];
