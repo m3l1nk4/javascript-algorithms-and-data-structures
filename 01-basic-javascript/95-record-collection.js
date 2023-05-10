511aa3f4d2d25b67b64f850917b6f4b2024a3832
メ <111341694+m3l1nk4@users.noreply.github.com>
2023-05-10 14:51:38 +0000

    Add files

---
+++ 01-basic-javascript/95-record-collection.js
@@ -0,0 +1,48 @@
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (value === '') {
+    delete records[id][prop];
+  } else if (prop !== 'tracks' && value !== '') {
+    records[id][prop] = value;
+  } else if (prop === 'tracks' && value !== '') {
+    if (!records[id].hasOwnProperty(prop)) {
+      records[id][prop] = [];
+    } 
+
+    records[id][prop].push(value);
+  }
+
+  return records;
+}
+
+// Tests
+// updateRecords(recordCollection, 1245, "tracks", "Addicted to Love");
+// updateRecords(recordCollection, 1245, "albumTitle", "Riptide");
+// updateRecords(recordCollection, 2468, "tracks", "Free");
+// updateRecords(recordCollection, 2548, "artist", "");
+// updateRecords(recordCollection, 2548, "tracks", "");
+// updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+// updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me");
+
+// console.log(recordCollection);
